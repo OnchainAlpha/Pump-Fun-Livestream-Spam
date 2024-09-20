32edd7531195242faa16f159f719300075f63ee8
Onchain Bureau <104756509+OnchainAlpha@users.noreply.github.com>
2024-09-20 21:15:22 +0200

    Update create_profile.py

--- create_profile.py
+++ create_profile.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import subprocess
 
 
-chrome_path = r'C:\Program Files\Google\Chrome\Application\chrome.exe'
+chrome_path = '/usr/bin/google-chrome'
 debugging_port = '--remote-debugging-port=8989'
 user_data_dir = '--user-data-dir={}'.format(Path.cwd() / 'profile')
 
